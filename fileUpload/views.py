30bc5bbc702a6b89b506ad0106e668187111444d
Mdkaifq <140621920+Mdkaifq@users.noreply.github.com>
2024-08-13 00:03:27 +0530

    Update views.py

--- fileUpload/views.py
+++ fileUpload/views.py
@@ -19,9 +19,9 @@ def uploadFile(request):
 
         combinedList = zip(df['Cust State'].tolist(), df['Cust Pin'].tolist(), df['DPD'].tolist())
         
-        return render(request, 'display.html', {'combinedList': combinedList})
+        return render(request, 'templates/display.html', {'combinedList': combinedList})
 
-    return render(request, 'fileUpload.html')
+    return render(request, 'templates/fileUpload.html')
 
 
 
@@ -31,13 +31,13 @@ def sendEmail(request):
         list2 = request.POST.getlist('list2[]')
         list3 = request.POST.getlist('list3[]')
 
-        # Combine the lists into a list of tuples
+        
         combined_list = zip(list1, list2, list3)
 
-        # Prepare the email content
-        email_body = render_to_string('mailTemplate.txt', {'combined_list': combined_list})
+        
+        email_body = render_to_string('templates/mailTemplate.txt', {'combined_list': combined_list})
 
-        # Send the email
+        
         email = EmailMessage(
             subject='Python Assignment - Md Kaif Qureshi',
             body=email_body,
@@ -49,6 +49,6 @@ def sendEmail(request):
         # Send the email
         email.send()
 
-        return render(request, 'display.html', {'alert_message': "email sent succesfully!"})
+        return render(request, 'templates/display.html', {'alert_message': "email sent succesfully!"})
 
-    return redirect('alert_message')  # Redirect if not POST request
+    return redirect('send_email')
